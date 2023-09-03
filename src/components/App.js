06f153948788b4e561ba5932c2bb25109001cdcc
Ivanna86Koval <ms.ivanna86@gmail.com>
2023-09-03 18:51:19 +0300

    updete App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,9 +7,9 @@ import { RootStyle } from './RootStyle/RootStyle.styled';
 
 export class App extends Component {
     state = {
-  good: 0,
-  neutral: 0,
-  bad: 0
+  Good: 0,
+  Neutral: 0,
+  Bad: 0
     }
 
     handleClickOptions = (option) => {
